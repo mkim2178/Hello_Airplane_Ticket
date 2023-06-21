e638890f85bb49c177bc30da037bd4d3a49cdd7a
mkim2178 <mkim2178@berkeley.edu>
2023-06-21 19:17:30 +0900

    Removed a function that prints out every male users

--- Backend/main.py
+++ Backend/main.py
@@ -8,18 +8,14 @@ import random
 app = FastAPI()
 
 
-USER_ID_AND_SEX = []
 EVERY_USER_DB_VERSION = []
+
+EVERY_USER = []
 
 
 @app.get("/")
 async def root():
     return {"hello": "world"}
-
-
-class IdSex(BaseModel):
-    id: str
-    sex: str
 
 
 class User(BaseModel):
@@ -56,9 +52,9 @@ def hash_the_password(user_password: str):
 
 
 def save_user(user_input: UserInput):
+    EVERY_USER.append(user_input)
+
     hashed_password = hash_the_password(user_input.user_pswd)
-    new_user = IdSex(id=user_input.user_id, sex=user_input.user_sex)  # append user's info as an object (only includes the user's id and sex) into a list: USER_ID_AND_SEX
-    USER_ID_AND_SEX.append(new_user)
     user_in_db = UserDB(**user_input.dict(), hashed_pswd=hashed_password, stored_date=datetime.now())  # append user's info as an object (db version) into a list: EVERY_USER_DB_VERSION
     EVERY_USER_DB_VERSION.append(user_in_db)
     print("User is successfully saved!")
@@ -74,28 +70,10 @@ async def create_user(user_input: UserInput):
 @app.get("/everyuser/")
 async def read_every_user_in_db_version() -> list:  # this is a private info but I'm doing this as a practice (user's hashed password should not be shared)
 
-    if not EVERY_USER_DB_VERSION:
+    if not EVERY_USER:
         return ["There is no user"]
 
-    return EVERY_USER_DB_VERSION
+    return EVERY_USER
 
-
-
-@app.get("/everymenuser/")
-async def read_only_men() -> list:  # this prints out every id who is man
-
-    if not USER_ID_AND_SEX:
-        return ["There is no user"]
-
-
-    man_lst = []
-    for user in USER_ID_AND_SEX:
-        if user.sex == "M":
-            man_lst.append(user.id)
-
-    if not man_lst:
-        return ["There is no man user"]
-
-    return man_lst
 
 
